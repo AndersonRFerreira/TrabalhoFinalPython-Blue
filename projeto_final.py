99756fc72e89751e883912f54432e35c35512eb8
AndersonRFerreira <andersonn.reis90@gmail.com>
2021-06-02 10:04:52 -0300

    Update projeto_final.py

--- projeto_final.py
+++ projeto_final.py
@@ -1,93 +1,108 @@
-from abc import ABC, abstractmethod
-from datetime import datetime
+from relogioPonto import UsuarioPonto, RelogioPonto
+from abc import ABC, abstractmethod #importa a biblioteca abstrata do python
+from datetime import datetime #importa a biblioteca datetime
+from os import system
+system('cls')
+
+class Pessoa(): #a calsse pessoa recebe a classe abstrata
+    class Pessoa:
+      @abstractmethod # É um método de criação ou construtores que aceitam objetos da mesma classe ou interface que uma classe atual.
+      def remuneracao(self): #função remuneração
+        pass          #Método de definição de bloco, para interpretação de código do python.
 
 
 
-class Pessoa(ABC):
-    @abstractmethod
-    def remuneracao(self):
-        pass
+# nome = str(input('Digite um nome: '))
+# idade = int(input('Digite a idade: '))
+# cpf = input('Digite o cpf: ')
 
-class Pedreiro():
+class Pedreiro(Pessoa): #Um exemplo da classe autônoma.
    
-   def __init__(self, nome):
-      self.__bonus = 1
-      # self.__diaria = 1
-      self.qtd_trabalhadores = 0
-      self.__nome = nome
+   def __init__(self):#Função criada com método especial que sempre é executado quando criamos uma instância de um objeto .
+      self.__bonus = 1      #Self parâmetro utilizado para invocar o objeto.
+      self.qtd_trabalhadores = 0 #Objeto que lista a quantidade de trabalhadores.
+      # self.__nome = nome = input('Digite o nome do Trabalhador: ') #Objeto que lista o nome do trabalhador.
       
    
    
    
-   def count_trabalhadores(self):
-      self.qtd_trabalhadores = int(input('Digite a quantidade de trabalhadores: '))
-      if self.qtd_trabalhadores != 0:
-         print(f'Temos {self.qtd_trabalhadores} trabalhando nessa obra.')
-      elif self.qtd_trabalhadores == 0:
-         print('O serviço foi concluido')
-      else:
-         print('Comando inválido.')
-   
-   
-   @property
-   def bonus(self):
-      return self.__bonus
+   def count_trabalhadores(self): #função que define a quantidade de trabalhadores
+        
+            self.qtd_trabalhadores = int(input('Quantos profissionais irão atuar: '))# Esse objeto recebe um input perguntando quantos trbalhadores há no local.
+            print()
+            if self.qtd_trabalhadores != 0: #Laço condicional, onde, se a quantidade de trabalhadores for diferente de 0 ele me traz o print abaixo.
+               print()
+               print(f'Profissionais atuando. {self.qtd_trabalhadores}.') #Mostre na tela quantos trabalhadores estão atuando no serviço.
+               print()
+            elif self.qtd_trabalhadores <= 0:#Enquanto a quantidade de trabalhadores for igual a 0
+               print()
+               print('O serviço foi concluido')#Diga que o serviço ja foi concluido.
+               print()
+            else:
+               print()
+               print('Comando inválido.')# Se digitar um número não compatível com a condição retorne número inválido.
+               
+   @property #Método que se refencia a um atributo.
+   def bonus(self): #função definir bonus, relativo as horas extras.
+      return self.__bonus #Retorne para mim o objeto listado
 
-   @property
-   def nome(self):
-      return self.__nome
+   @property #Método que se refencia a um atributo.
+   def nome(self): #função nome
+      return self.__nome #Me retorne o objeto nome.
    
-   @bonus.setter
-   def bonus(self, bonus):
-      try:
-         self.__bonus = float(bonus)
-      except:
+   @bonus.setter #Usei para fazer uma ligação feita com meu @property.
+   def bonus(self, bonus):#função bonus, referente as horas extras trabalhadas.
+      try:#Ele vai tentar pegar o parâmetro bonus, e convertê-lo para flot
+         self.__bonus = float(bonus)#O try vai fazer o tratamento para que o self.bonus vire um float mesmo ele sendo um float.
+      except:#qualquer execeção, ou seja, qualquer coisa que acontecer diferente do que o try pede, ele irá retornar o print abaixo.
          print('O valor do bonus não é está correto. Por favor insira um valor valido')
 
-   # def adicionar_diaria(self, diaria):
-   #    self.__diaria  += diaria
-   # def reset_diaria(self):
-   #    self.__diaria = 0
-   
-   
-   #    salario_bruto = round((quantidade_horas_trabalhadas + minutos_em_decimal) * valor_por_hora, 2)
-   #    ir = round(salario_bruto*11/100, 2)
-   #    inss = round(salario_bruto*8/100, 2)
-
-class Diaria:
-   def __init__ (self):
-      # self.valor = None
+nome = str(input('Nome do trabalhador: '))
+idade = int(input('Digite a idade: '))
+cpf = input('Digite o cpf: ')
+print()
+contratante = str(input('Nome do contratante: '))
+cpf2 = input('Digite o cpf: ')
+class Diaria: #classe diária
+   def __init__ (self):#Função criada com método especial que sempre é executado quando criamos uma instância de um objeto .
+      self.nome = nome
+      self.idade = idade
+      self.cpf = cpf
+      self.contratante = contratante
+      self.cpf2 = cpf2
       self.inicio = 0
       self.pausa_almoco = 0
       self.fim_almoco = 0
       self.fim_expediente = 0
-      self.va_hora = float(input("Digite o quanto você ganha por hora: "))
-      self.ho_trabalhadas = int(input("Digite o número de horas trabalhadas no mês: "))
-      self.m_trabalhados = int(input("Digite o número de minutos trabalhados no mês (além das horas): "))
+      self.va_hora = float(input("Digite o quanto ele ganha por hora: "))
+      print()
+      self.ho_trabalhadas = int(input("Quantidade de horas trabalhadas no mês: "))
+      print()
+      self.m_trabalhados = int(input("Quantidade minutos trabalhados no mês (hora extra): "))
       self.minutos_em_decimal = 0
       self.ir = 0
       self.inss = 0
       self.salario_bruto = 0
       self.salario_liquido = 0
       self.diaria = 1
-   def iniciar_trabalho(self, inicio_expediente):
+   def iniciar_trabalho(self, inicio_expediente): #função que define o íncio da jornada de trabalho.
       self.inicio = datetime.strptime(inicio_expediente, '%H:%M' )
       
-   def saida_almoco(self, saida_almoco):
+   def saida_almoco(self, saida_almoco):#função que define o horário de almoço
       self.pausa_almoco = datetime.strptime(saida_almoco, '%H:%M' )
 
-   def volta_almoco(self, volta_almoco):
+   def volta_almoco(self, volta_almoco):#função que define o fim do horário de almoço.
       self.fim_almoco = datetime.strptime(volta_almoco, '%H:%M' )
    
-   def encerrar_trabalho(self, fim_expediente):
+   def encerrar_trabalho(self, fim_expediente):# função que define o fim do expediente. 
       self.fim_expediente = datetime.strptime(fim_expediente, '%H:%M' )
      
          
-   def horas_trabalhadas(self):
+   def horas_trabalhadas(self):#função que irá calcular as horas trabalhadas.
       alfa = (self.__fim_expediente - self.saida__almoco - self.volta__almoco - self.__inicio_expediente).seconds
       return round((alfa/60/60))
    
-   def remuneracao(self):
+   def remuneracao(self):# função que fará todo o calculo do salário, incluindo as taxas e as impimindo um holerith como parecido com uma nota fiscal paulista
          if self.diaria > 0:
             self.minutos_em_decimal = round(self.m_trabalhados / 60, 2)
 
@@ -96,12 +111,25 @@ class Diaria:
             self.inss = round(salario_bruto*8/100, 2)
             self.sindicato = round(salario_bruto*5/100, 2)
             self.salario_liquido = round(salario_bruto - self.ir -self.inss - self.sindicato, 2)
-
+         print()
+         print('Holerith')
+         print()
+         print(f'Dados do contratante')
+         print(f'{self.contratante} portador do cpf {self.cpf2}')
+         print()
+         print('Dados do contratado')
+         print(f'{self.nome} idade {self.idade} portador do cpf {self.cpf}')
+         print('Nos dados abaxo contém, as informações sobre os descontos na sua folha de pagamento.')
+         print()
+         print(f'Total de horas trabalhadas = {self.ho_trabalhadas}')
          print(f" + Salário Bruto : R$", salario_bruto)
          print(f" - IR (11%) : R$", {self.ir})
          print(f" - INSS (8%) : R$", {self.inss})
          print(f" - Sindicato (5%) : R$", {self.sindicato})
          print(f" = Salário Liquido : R$", {self.salario_liquido})
+         print()
+         print('A informação contida neste documento é para uso único e exclusivo da pessoa a quem se destina.')
+         print()
    @property
    def valor_hora(self):
       return self.valor_hora
@@ -111,7 +139,7 @@ class Diaria:
       self.__diaria = 0
    
 
-p1 = Pedreiro('Negão do Concreto')
+p1 = Pedreiro()
 
 p1.bonus = 1.2
 p1.count_trabalhadores()
@@ -121,6 +149,6 @@ d1.iniciar_trabalho('09:00')
 d1.saida_almoco('13:00')
 d1.volta_almoco('14:15')
 d1.encerrar_trabalho('17:00')
-#p1.adicionar_diaria() 
+
 
 d1.remuneracao()
